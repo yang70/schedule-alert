ed8546b8e26cf3e2ce7a0b91543400b5efff99fb
Matthew Yang <yang70@gmail.com>
2026-02-15 16:23:20 -0800

    Deploy script

--- bin/deploy-remote.sh
+++ bin/deploy-remote.sh
@@ -7,20 +7,20 @@ ssh ubuntu@44.226.63.157 << 'ENDSSH'
   cd ~/schedule
   echo "📥 Pulling latest code..."
   git pull
-  
+
   echo "📦 Installing dependencies..."
   bundle install --without development test
   npm install
-  
+
   echo "🎨 Precompiling assets..."
   RAILS_ENV=production bundle exec rails assets:precompile
-  
+
   echo "🗄️  Running migrations..."
   RAILS_ENV=production bundle exec rails db:migrate
-  
+
   echo "♻️  Restarting services..."
   sudo systemctl restart tourneyping-web tourneyping-worker
-  
+
   echo "✅ Deployment complete!"
 ENDSSH
 
